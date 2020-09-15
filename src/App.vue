5e9cf49885a37d79c72df1fbe7893c9743e62c3d
Mikołaj Lechtański <mikolec@gmail.com>
2020-09-15 22:56:56 +0200

    Created project from 'Vue.js: Building an Interface' linkedin course

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main-app" class="container">
+    <div class="row justify-content-center">
+      <AddAppointment @add="addItem" />
+      <SearchAppointments
+        @searchRecords="searchItems"
+        :filterKey="filterKey"
+        :filterDir="filterDir"
+        @changeFilterKey="changeFilterKey"
+        @changeFilterDir="changeFilterDir"
+      />
+      <AppointmentList :appointments="filteredApts" @remove="removeItem" @edit="editItem" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import _ from "lodash";
+import AppointmentList from "@/components/AppointmentList.vue";
+import AddAppointment from "@/components/AddAppointment.vue";
+import SearchAppointments from "@/components/SearchAppointments.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "MainApp",
+  data() {
+    return {
+      appointments: [],
+      aptIndex: 0,
+      searchTerms: "",
+      filterKey: "petName",
+      filterDir: "asc",
+    };
+  },
+  computed: {
+    searchedApts() {
+      return this.appointments.filter((item) => {
+        return (
+          item.petName.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.petOwner.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.aptNotes.toLowerCase().match(this.searchTerms.toLowerCase())
+        );
+      });
+    },
+    filteredApts() {
+      return _.orderBy(
+        this.searchedApts,
+        (item) => {
+          return item[this.filterKey].toLowerCase();
+        },
+        this.filterDir
+      );
+    },
+  },
+  methods: {
+    addItem(apt) {
+      apt.aptId = this.aptIndex;
+      this.aptIndex++;
+      this.appointments.push(apt);
+    },
+    removeItem(apt) {
+      // this.appointments = _.without(this.appointments, apt);
+      this.appointments = this.appointments.filter((item) => item != apt);
+    },
+    editItem(id, field, text) {
+      //make sure we are getting an element in the appointments list...
+      // const aptIndex = _.findIndex(this.appointments, { aptId: id });
+      var aptIndex = this.appointments.findIndex((item) => (item.aptId = id));
+      this.appointments[aptIndex][field] = text;
+    },
+    searchItems(terms) {
+      this.searchTerms = terms;
+    },
+    changeFilterKey(key) {
+      this.filterKey = key;
+    },
+    changeFilterDir(dir) {
+      this.filterDir = dir;
+    },
+  },
+  components: { AppointmentList, AddAppointment, SearchAppointments },
+  mounted() {
+    axios.get("./data/appointments.json").then(
+      (response) =>
+        (this.appointments = response.data.map((item) => {
+          item.aptId = this.aptIndex;
+          this.aptIndex++;
+          return item;
+        }))
+    );
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SearchAppointments.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="col-12 col-md-10 col-lg-7">
+    <div class="input-group my-3">
+      <input
+        id="SearchApts"
+        placeholder="Search"
+        type="text"
+        class="form-control"
+        aria-label="Search Appointments"
+        v-model="searchTerm"
+      />
+
+      <div class="input-group-append">
+        <button
+          type="button"
+          class="btn btn-primary dropdown-toggle"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          Sort by
+          <span class="caret"></span>
+        </button>
+
+        <div class="dropdown-menu dropdown-menu-right">
+          <a
+            href="#"
+            class="dropdown-item d-flex justify-content-between"
+            id="petName"
+            @click="$emit('changeFilterKey', 'petName')"
+          >
+            Pet Name
+            <font-awesome-icon icon="check" v-if="filterKey == 'petName'" />
+          </a>
+
+          <a
+            class="dropdown-item d-flex justify-content-between"
+            href="#"
+            id="aptDate"
+            @click="$emit('changeFilterKey', 'aptDate')"
+          >
+            Date
+            <font-awesome-icon icon="check" v-if="filterKey == 'aptDate'" />
+          </a>
+
+          <a
+            href="#"
+            class="dropdown-item d-flex justify-content-between"
+            id="ownerName"
+            @click="$emit('changeFilterKey', 'petOwner')"
+          >
+            Owner
+            <font-awesome-icon icon="check" v-if="filterKey == 'petOwner'" />
+          </a>
+
+          <div class="dropdown-divider" role="separator"></div>
+
+          <a
+            class="dropdown-item d-flex justify-content-between"
+            href="#"
+            id="asc"
+            @click="$emit('changeFilterDir', 'asc')"
+          >
+            Asc
+            <font-awesome-icon icon="check" v-if="filterDir == 'asc'" />
+          </a>
+
+          <a
+            class="dropdown-item d-flex justify-content-between"
+            href="#"
+            id="desc"
+            @click="$emit('changeFilterDir', 'desc')"
+          >
+            Desc
+            <font-awesome-icon icon="check" v-if="filterDir == 'desc'" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: "SearchAppointments",
+  props: ["filterKey", "filterDir"],
+  data() {
+    return {
+      searchTerm: "",
+    };
+  },
+  watch: {
+    searchTerm(value) {
+      this.$emit("searchRecords", value);
+    },
+  },
+  components: { FontAwesomeIcon },
+};
+</script>
+
+<style></style>
